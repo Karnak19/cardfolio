b6b1af0d03159b131266afae307e6bf5072d6535
Basile Vernouillet <basile64.v@gmail.com>
2021-10-09 23:21:13 +0200

    remove google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,21 +9,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-HC7W9WGC9G`} />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-HC7W9WGC9G', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        </Head>
         <body
           style={{
             backgroundImage: "url('/images/bg.svg')",
